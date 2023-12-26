b17a00b060217ab096ea9732c8ff3d5f0dc9d5a9
Quinteroo <146204443+Quinteroo@users.noreply.github.com>
2023-12-26 16:27:08 +0100

    Add files via upload

---
+++ CODEWARS/7-kyu/09-filtering.js
@@ -0,0 +1,28 @@
+
+
+// DESCRIPTION:
+// In this kata you will create a function that takes a list of non-negative integers and strings 
+// and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+
+//! cuando hacemos un typeof, nos devuelve un STRING con el tipo de dato
+function filter_list(l) {
+  let arrayNums = [];
+  for (const x of l) {
+    if (typeof x === 'number') {
+      arrayNums.push(x)
+    }
+  }
+  return arrayNums;
+}
+
+
+//! otra solución refactorizada
+function filter_list(l) {
+  return l.filter((obj) => typeof obj === 'number')
+}
